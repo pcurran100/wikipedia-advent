30701157b1488c3c7a8a6b7d950dbb3f34637bec
Patrick Curran <pcurran100@gmail.com>
2025-12-01 18:42:15 +0000

    changed config to buidl with actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const repoBase = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: { unoptimized: true },
+  basePath: repoBase,
+  assetPrefix: repoBase,
 };
 
 export default nextConfig;
